974dee664a35435e40a134b1e35f7a65fa84eb84
lucas.ws <fhdufhdu@gmail.com>
2023-05-20 22:02:59 +0900

    [#1] node 적용

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+const srcDir = path.join(__dirname, "src");
+const scriptDir = path.join(srcDir, "scripts");
+const htmlDir = path.join(srcDir, "html");
+const distDir = path.join(__dirname, "dist");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    addImageBtn: path.join(scriptDir, "add-image-button.js"),
+  },
+  output: {
+    path: path.join(distDir, "scripts"),
+    filename: "[name].js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: false,
+      filename: path.join(distDir, "html", "options.html"),
+      template: path.join(htmlDir, "options.html"),
+    }),
+  ],
+  devtool: "inline-source-map",
+};
