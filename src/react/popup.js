c3dc9975db8225b0ce6a8a5139e08b9359c0e796
Juhye Son <juhye.son990902@gmail.com>
2023-05-21 00:16:23 +0900

    feat: 팝업에 설정창 바로가기 버튼 추가

--- src/react/popup.js
+++ src/react/popup.js
@@ -1,12 +1,41 @@
 import React from "react";
 import vemacLogo from "./vemac_logo.png";
+import styled from "styled-components";
 
 const Popup = () => {
+  const myid = chrome.runtime.id;
+
+  const openInNewTab = (url) => {
+    const newWindow = window.open(url, "_blank", "noopener,noreferrer");
+    if (newWindow) newWindow.opener = null;
+  };
   return (
-    <div>
-      <img src={vemacLogo}></img>
-    </div>
+    <Wrapper>
+      <Logo src={vemacLogo}></Logo>
+      <div
+        onClick={() =>
+          openInNewTab(`chrome-extension://${myid}/dist/setting.html`)
+        }
+      >
+        설정창으로 이동하기
+      </div>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  width: 350px;
+  height: 350px;
+
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const Logo = styled.img`
+  width: 100px;
+  height: 100px;
+`;
 
 export default Popup;
